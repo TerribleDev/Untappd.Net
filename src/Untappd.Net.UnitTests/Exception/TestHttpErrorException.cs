e3643c692900ab45573649eda69f6a60ce0d737a
Tommy Parnell <tparnell8@gmail.com>
2015-06-19 22:29:35 -0500

    basic if case cover

---
+++ src/Untappd.Net.UnitTests/Exception/TestHttpErrorException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using Untappd.Net.Exception;
+using Moq;
+using RestSharp;
+
+
+namespace Untappd.Net.UnitTests.Exception
+{
+    [TestFixture]
+    public class TestHttpErrorException
+    {
+        [Test]
+        public void ConfirmNullExceptions()
+        {
+            var mockRequest = new Mock<IRestRequest>();
+            var mockResponse = new Mock<IRestResponse>();
+           
+            Assert.Throws<ArgumentNullException>(()=>{
+                    var t = new HttpErrorException(mockRequest.Object, null);
+                    Console.WriteLine(t);
+
+                });
+            Assert.Throws<ArgumentNullException>(()=>{
+                    var t = new HttpErrorException(null, mockResponse.Object);
+                    Console.WriteLine(t);
+
+                });
+        }
+    }
+}
+
